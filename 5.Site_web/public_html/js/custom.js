455d0aaaf55a37a65e3e2c94c0e2eb9b0d5bfaab
Oleh Skrypnykov <oskripnikov@gmail.com>
2020-07-20 22:05:09 +0300

    add Site_web 3

--- 5.Site_web/public_html/js/custom.js
+++ 5.Site_web/public_html/js/custom.js
@@ -1,11 +1,21 @@
-$('.navbar-collapse a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
+$('.navbar-collapse a').on('click',function(){
+    $(".navbar-collapse").collapse('hide');
+  });
 
-$(window).scroll(function() {
-    if($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+  $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+          $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+  });
+
+$('.home-slider').owlCarousel({
+    animateOut:'fadeOut',
+    items:1,
+    loop:true,
+    dots:false,
+    autoplayHoverPause:false,
+    autoplay:true,
+    smartSpeed: 1000,
+  })
