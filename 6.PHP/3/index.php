db7a9e04d2937653f0081f7cbb3b1b0a91e4ff5a
Oleh Skrypnykov <oskripnikov@gmail.com>
2020-07-27 11:13:21 +0300

    add PHP 3/22

---
+++ 6.PHP/3/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Script 3</title>
+</head>
+<body>
+    <?php
+      /* Начинаю комментарий на несколько строк,
+      ниже определены несколько переменных */
+      $name="Анна";
+      $ages=22;
+      $weight=77;
+      echo"<br />";
+      # Однострочный комментарий
+      echo "$name";
+      echo"<br />";
+      echo "$ages года";
+      echo"<br />";
+      // и здесь однострочный комментарий
+      echo "и имеет вес $weight кг";
+    ?>  
+</body>
+</html>
