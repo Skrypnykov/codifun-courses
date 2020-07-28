d6f358f5ca96e14ab526f45a27a500eadb4f79b9
Oleh Skrypnykov <oskripnikov@gmail.com>
2020-07-28 17:56:14 +0300

    add PHP 4/22

---
+++ 6.PHP/4/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Script 4</title>
+</head>
+<body>
+    <?php
+      $ages = 27;
+      echo $ages;
+      echo "<br />";
+      echo var_dump($ages);
+      echo "<br />";
+      $temp = -10;
+      echo $temp;
+      echo "<br />";
+      echo var_dump($temp);
+      echo "<br />";
+    ?>  
+</body>
+</html>
---
+++ 6.PHP/4/index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Script 4</title>
+</head>
+<body>
+    <?php
+      define("companyName","CodiFun");
+      echo companyName;
+    ?>  
+</body>
+</html>
